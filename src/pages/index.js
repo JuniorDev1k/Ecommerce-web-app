3cc43a557668e857385ccff7fb30462e3d1eab4e
JuniorDev1k <ayoubdev1k@gmail.com>
2023-12-15 00:24:24 +0100

    Dashboard-v1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 // the entry point of pages folder
 // herer we import all the pages
 
-import Dashboard from "./AdminDashboard";
 import Home from "./Home";
 import Error from "./Error";
-import About from "../components/About";
+// import About from "../components/About";
 import Products from "./Products";
 import SignlProduct from "./SignlProduct";
 import Signup from "./Signup";
 import Login from "./Login";
-export { Dashboard, Home, Error, About, Products, SignlProduct, Login, Signup };
+export { Home, Error, Products, SignlProduct, Login, Signup };
