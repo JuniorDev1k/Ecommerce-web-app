0acfdbf53d5567f260b9f3431e91038a62e49e52
JuniorDev1k <ayoubdev1k@gmail.com>
2024-01-15 12:26:35 +0100

    styling

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -10,47 +10,34 @@ const Services = () => {
   }, []);
   return (
     <>
-      <div className=" services-container min-h-screen">
-        <section className="flex p-20 gap-5 max-sm:flex-col max-sm:items-center">
-          <div className="felx w-1/2  " data-aos="zoom-in">
-            <div className=" a2  flex justify-center gap-6  max-sm:flex-col ">
-              <Hexagone hex="HIGHT QUALITY PRODUCTS" />
-              <Hexagone hex="HAVE FUN" />
-            </div>
-            <div className="a2 flex justify-center gap-16 md:items-center max-md:gap-6 max-sm:flex-col">
-              <Hexagone hex="CUSTOM STOR FUNCTIONALITIES" />
-              <div className="border-b-4 border-red-400 rounded pb-1 max-md:hidden ">
-                <Logo />
-              </div>
-              <Hexagone hex="HAVE FUN" />
-            </div>
-            <div className="a2 flex justify-center gap-6 max-md:gap-6 max-sm:flex-col">
-              <Hexagone hex="HAVE FUN" />
-              <Hexagone hex="HAVE FUN" />
-            </div>
+      <section className=" services-container min-h-screen flex p-16 gap-10 max-sm:flex-col max-sm:items-center ">
+        <div className="felx w-1/2  " data-aos="zoom-in">
+          <div className=" a2  flex justify-center gap-6  max-sm:flex-col ">
+            <Hexagone hex="HIGHT QUALITY PRODUCTS" />
+            <Hexagone hex="HAVE FUN" />
           </div>
-          {/* left part  ---------------------------------------------------*/}
-          <div className="flex flex-col w-1/2 justify-between  p-20">
-            <h1 className="servicesTtitle  text-3xl mt-2 font-robotto">
-              OUR SERVICES
-            </h1>
-            <div className="trustworthy flex flex-col gap-2 text-lg border-l-4 border-red-800  p-5 font-poppins  ">
-              <div className="flex gap-4">
-                <h1 className="font-bold text-secondary  "># 1</h1>
-                <h1 className="text-text">OUE CLIENTS OUR PRIORITY</h1>
-              </div>
-              <div className="flex gap-4">
-                <h1 className="font-bold text-secondary "># 2</h1>
-                <h1 className="text-text">OUE CLIENTS OUR PRIORITY</h1>
-              </div>
-              <div className="flex gap-4">
-                <h1 className="font-bold text-secondary "># 3</h1>
-                <h1 className="text-text">OUE CLIENTS OUR PRIORITY</h1>
-              </div>
+          <div className="a2 flex justify-center gap-16 md:items-center max-md:gap-6 max-sm:flex-col">
+            <Hexagone hex="CUSTOM STOR FUNCTIONALITIES" />
+            <div className="border-b-4 border-red-800 rounded pb-1 max-lg:hidden  ">
+              <Logo />
             </div>
+            <Hexagone hex="HAVE FUN" />
           </div>
-        </section>
-      </div>
+          <div className="a2 flex justify-center gap-6 max-md:gap-6 max-sm:flex-col">
+            <Hexagone hex="HAVE FUN" />
+            <Hexagone hex="HAVE FUN" />
+          </div>
+        </div>
+        {/* left part  ---------------------------------------------------*/}
+        <div className="w-1/2 flex flex-col items-center justify-center pb-20   ">
+          <h1 className="servicesTtitle  text-4xl mt-2 font-robotto mb-4  ">
+            OUR SERVICES
+          </h1>
+          <h3 className="text-white font-robotto  text-2xl">
+            An Overview of what we offer to our clients!
+          </h3>
+        </div>
+      </section>
     </>
   );
 };
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Auth } from "../config/firebase";
-import { signOut } from "firebase/auth";
+
 import FeaturedProducts from "../components/FeaturedProducts";
 import FilterSideBar from "../components/FilterSideBar";
 import ProductsUserProfile from "../components/ProductsUserProfile";
 import ProductsGrid from "../components/ProductsGrid";
 
 const Products = () => {
-  const Navigate = useNavigate();
-  const signOutuser = async () => {
-    await signOut(Auth);
-    Navigate("/Login");
-  };
   return (
     <>
       <FeaturedProducts />
@@ -27,7 +20,7 @@ const Products = () => {
         </section>
         <div>
           <h1>Log our bro common u have to leave this page</h1>
-          <button onClick={signOutuser}>LogOut</button>
+          <button>signout</button>
         </div>
       </div>
     </>
