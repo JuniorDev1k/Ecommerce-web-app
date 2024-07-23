81dc65580719d9ef78f8aa42d91b6a2c1dc7505a
JuniorDev1k <ayoubdev1k@gmail.com>
2024-07-23 18:33:38 +0200

    changes-befor-reDeploy

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
 
-          <Route
-            path="/Products"
-            element={<ForProducts element={<Products />} />}
-          />
+          <Route path="/Products" element={<Products />} />
 
           <Route path="Products/:productId" element={<SignlProduct />} />
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Button = (props) => {
   const text = props.button;
+  const navigate = useNavigate();
   return (
-    <button className="btn btn-xs  bg-red-600 sm:btn-sm md:btn-md lg:btn-lg">
+    <button
+      onClick={() => navigate("/Products")}
+      className="btn btn-xs  bg-red-600 sm:btn-sm md:btn-md lg:btn-lg"
+    >
       Search Products
     </button>
   );
