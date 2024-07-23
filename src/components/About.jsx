58f526b38a417c3788ae4aad52942a63245e1dd8
JuniorDev1k <ayoubdev1k@gmail.com>
2024-07-23 17:53:27 +0200

    styling-about-section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,15 +12,15 @@ const About = () => {
 
   return (
     <>
-      <div className=" h-screen flex pl-20 bg-background relative  ">
+      <div className=" h-screen flex pl-10 bg-background relative  ">
         <AboutLeft object={currentYEAR} />
         {/* listing the year at the rights  */}
-        <div className=" flex  flex-col text-text absolute top-0 right-0 justify-between h-4/5">
+        <div className=" flex  flex-col text-text absolute top-0 right-0 justify-between h-screen py-4">
           {Dabout.map((y) => {
             const { year, id } = y;
             return (
               <button
-                className="hover:text-secondary h-1/6 rounded-l"
+                className="hover:text-secondary  rounded-l-md hover:bg-white p-4 transition "
                 key={id}
                 onClick={() => selectYear(id)}
               >
--- src/components/AboutLeft.jsx
+++ src/components/AboutLeft.jsx
@@ -5,20 +5,24 @@ const AboutLeft = (props) => {
   const { img, story, year } = props.object;
 
   return (
-    <div className="flex  w-3/4 items-center ">
-      <div className="  w-full ">
-        <h1 className=" font-robotto text-center text-xl text-text fw-bold mb-20">
+    <div className=" w-3/4   flex flex-col sm:flex-row items-center gap-6">
+      <div className=" w-full pt-6   flex flex-col  items-center">
+        <h1 className=" font-robotto  text-2xl text-text pb-2 tracking-widest	 ">
           ABOUT US
         </h1>
-        <div className="border-2 border-text mr-2 ">
-          <img src={img} alt="img-of-us" className="" />
+        <div className="w-80 h-80 sm:h-[25rem]  sm:w-[25rem]">
+          <img
+            src={img}
+            alt="img-of-us"
+            className="w-full h-full object-cover   rounded-sm "
+          />
         </div>
 
         <div />
       </div>
-      <article className=" text-text ">
+      <article className=" text-text text-center font-poppins  ">
         <h1 className=" font-poppins  fw-bold  text-2xl mb-4 ">{year}</h1>
-        <p className=" font-Nunito text-sm tracking-wider leading-6 text-accent ">
+        <p className=" font-Nunito text-lg tracking-wider leading-6 text-text  ">
           {story}
         </p>
       </article>
