c99806c54178897b8266bbbe705f55abd4d26df0
JuniorDev1k <ayoubdev1k@gmail.com>
2024-07-30 12:20:17 +0200

    admin-add-product-UI

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -105,110 +105,146 @@ const AddProduct = () => {
           <span>Product Added!</span>
         </div>
       )}
-      <div className="form-product flex flex-col items-center justify-center p-2 ">
+      <div className="form-product flex flex-col items-center justify-center p-2 mt-20 ">
         {err && <h1 className="text-red 500">{err}</h1>}
-        <h1 className="mb-10 text-2xl text-red-500  ">Admin Dashboard</h1>
+        <h1 className="text-center text-2xl font-medium font-robotto -tracking-tighter   ">
+          Add Your Product
+        </h1>
         <form
           onSubmit={AddProduct}
-          className={`flex flex-col gap-4    p-2 w-1/2`}
+          className={`grid grid-cols-2 gap-4 p-4 w-full`}
         >
-          <h1 className="text-center ">Add you Prudct</h1>
-          <label htmlFor="title">Product Title</label>
-          <input
-            type="text"
-            placeholder="Product Title"
-            onChange={handlchange}
-            name="name"
-            required
-          />
-          <label htmlFor="description">description</label>
-          <input
-            type="text"
-            placeholder="Product Description"
-            onChange={handlchange}
-            name="subdescrb"
-            required
-          />
-          <label htmlFor="reviews">reviews</label>
-          <input
-            type="number"
-            placeholder="reviews"
-            onChange={handlchange}
-            name="reviews"
-            required
-          />
-          <div className="input-category">
-            <select name="category" onChange={handlchange}>
-              {categorise.map((i) => (
-                <option key={i.value} value={i.value}>
-                  {i.name}
-                </option>
-              ))}
-            </select>
+          <div className=" flex  gap-20 flex-col left-form">
+            <div className="flex flex-col h-[17rem] align-center ">
+              <label htmlFor="">Image</label>
+              <input
+                type="file"
+                name="ImgUrl"
+                onChange={(e) => setFile(e.target.files[0])}
+                className="border-none ml-20 "
+              />
+              <img
+                src={file ? URL.createObjectURL(file) : Noimg}
+                alt={file ? "not yet" : "letgsgosogso"}
+                className="w-full h-full border-2 border-solid border-black "
+              />
+            </div>
+
+            <div className="Pr-dettials grid grid-cols-2  ">
+              <div className="grid grid-cols-2 gap-4 items-center">
+                <div>
+                  <label htmlFor="reviews">reviews</label>
+                  <input
+                    type="number"
+                    placeholder="reviews"
+                    onChange={handlchange}
+                    name="reviews"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="Available">Available</label>
+                  <input
+                    type="checkbox"
+                    onChange={handlchange}
+                    name="available"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="rating">Rating</label>
+                  <input
+                    type="number"
+                    placeholder="rating"
+                    onChange={handlchange}
+                    name="rating"
+                    required
+                  />
+                </div>
+                <div>
+                  <label htmlFor="">color</label>
+                  <div className="input-color">
+                    <select name="color" onChange={handlchange}>
+                      {colorOptions.map((C) => (
+                        <option
+                          style={{ backgroundColor: C.value }}
+                          key={C.value}
+                          value={C.value}
+                        >
+                          {C.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="input-category flex flex-col gap-2">
+                <label htmlFor="">Category</label>
+                <select name="category" onChange={handlchange}>
+                  {categorise.map((i) => (
+                    <option key={i.value} value={i.value}>
+                      {i.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
           </div>
-          <label htmlFor="price">Price</label>
-          <input
-            type="number"
-            placeholder="$"
-            onChange={handlchange}
-            name="price"
-            required
-          />
-          <label htmlFor="Available">Available</label>
-          <input type="checkbox" onChange={handlchange} name="available" />
-          <label htmlFor="rating">Rating</label>
-          <input
-            type="number"
-            placeholder="rating"
-            onChange={handlchange}
-            name="rating"
-            required
-          />
-          <label htmlFor="Descriptionbig">Sub Description</label>
-          <textarea
-            name="description"
-            className="border-2 border-t-gray-900"
-            id=""
-            cols="2"
-            rows="2"
-            onChange={handlchange}
-            required
-          ></textarea>
-          <div className="flex gap-2">
-            <label htmlFor="">Image</label>
-            <input
-              type="file"
-              name="ImgUrl"
-              onChange={(e) => setFile(e.target.files[0])}
-            />
-            <img
-              src={file ? URL.createObjectURL(file) : Noimg}
-              alt={file ? "not yet" : "letgsgosogso"}
-            />
-          </div>
-          <label htmlFor="">color</label>
-          <div className="input-color">
-            <select name="color" onChange={handlchange}>
-              {colorOptions.map((C) => (
-                <option
-                  style={{ backgroundColor: C.value }}
-                  key={C.value}
-                  value={C.value}
-                >
-                  {C.name}
-                </option>
-              ))}
-            </select>
+          <div className="right-form">
+            <div>
+              <div>
+                <label htmlFor="title">Product Title</label>
+                <input
+                  type="text"
+                  placeholder="Product Title"
+                  onChange={handlchange}
+                  name="name"
+                  required
+                />
+              </div>
+              <div>
+                {" "}
+                <label htmlFor="price">Price</label>
+                <input
+                  type="number"
+                  placeholder="$"
+                  onChange={handlchange}
+                  name="price"
+                  required
+                />
+              </div>
+              <div>
+                {" "}
+                <label htmlFor="description">description</label>
+                <input
+                  type="text"
+                  placeholder="Product Description"
+                  onChange={handlchange}
+                  name="subdescrb"
+                  required
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="Descriptionbig">Sub Description</label>
+              <textarea
+                name="description"
+                className="border-2 border-t-gray-900"
+                id=""
+                cols="2"
+                rows="2"
+                onChange={handlchange}
+                required
+              ></textarea>
+            </div>
           </div>
           <button
-            className=" ml-40 rounded-xl p-2 bg-black w-1/2 text-white"
+            className="  mt-20 rounded-xl p-2 bg-black w-1/2 text-white"
             type="submit"
           >
             Create Product
           </button>
         </form>
       </div>
-      <div>{/* {file && <img src={file.name} alt="img" />} */}</div>
     </>
   );
 };
